e53e75c6627b7b8ba800c70c4f3ef2da23bd1cf3
yiminglien@mirle.com.tw <yiminglien@mirle.com.tw>
2019-04-25 13:58:15 +0800

    Design and Test OK

---
+++ DelegateNotice/TemperatureMonitorUsingDelegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateNotice
+{
+    public class TemperatureMonitorUsingDelegate
+    {
+        public delegate void TemperatureChangedHandler(double tempature);
+
+        public TemperatureChangedHandler OnTemperatureChanged;
+
+        private double temperature;
+
+        public double Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                var oldTemperature = temperature;
+                if (oldTemperature != value)
+                {
+                    temperature = value;
+                    OnTemperatureChanged.Invoke(value);
+                }
+            }
+        }
+
+        public TemperatureMonitorUsingDelegate()
+        {
+            // 使用delegate必須給定一個初始的委派方法
+            OnTemperatureChanged = temperatureChanged;
+        }
+
+        private void temperatureChanged(double tempature)
+        {
+            Console.WriteLine($"溫度發生變化了...{tempature}");
+        }
+    }
+}
---
+++ DelegateNoticeTests/Form1Tests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using DelegateNotice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateNotice.Tests
+{
+    [TestFixture()]
+    public class Form1Tests
+    {
+        [Test()]
+        public void DelegateTest()
+        {
+            // 使用Delegate完成Observer pattern
+            Console.WriteLine("Delegate Demo");
+            DesktopApp desktopApp = new DesktopApp();
+            MobileApp mobileApp = new MobileApp();
+
+            TemperatureMonitorUsingDelegate temperatureMonitorUsingDelegate = new TemperatureMonitorUsingDelegate();
+
+            temperatureMonitorUsingDelegate.OnTemperatureChanged += desktopApp.OnTemperatureChanged;
+            temperatureMonitorUsingDelegate.OnTemperatureChanged += mobileApp.OnTemperatureChanged;
+
+            Console.WriteLine("溫度變化了，現在是30.5度");
+            temperatureMonitorUsingDelegate.Temperature = 30.5;
+
+            Console.WriteLine("溫度沒變化，現在依然是30.5度");
+            temperatureMonitorUsingDelegate.Temperature = 30.5;
+
+            Console.WriteLine("溫度變化了，現在是28.6度");
+            temperatureMonitorUsingDelegate.Temperature = 28.6;
+
+            Console.WriteLine("mobileApp不再想觀察了");
+            temperatureMonitorUsingDelegate.OnTemperatureChanged -= mobileApp.OnTemperatureChanged;
+
+            Console.WriteLine("溫度變化了，現在是27.6度");
+            temperatureMonitorUsingDelegate.Temperature = 27.6;
+            Console.WriteLine();
+
+            Assert.True(true);
+        }
+    }
+}
